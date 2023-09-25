8c55951a26dc49842d05281021e8cc1ca8200f2c
gliv <cliffdl@pm.me>
2023-09-25 17:53:06 +0200

    remove sanity leftovers

---
+++ src/types/index.ts
@@ -0,0 +1,10 @@
+///////
+
+// What document types do we need?
+// task
+// user
+// team
+// labels (user can pick from set list, can only be one)
+// tags (user can create new ad-hoc, can be multiple)
+
+// https://www.sanity.io/docs/reference-type
--- src/types/tasks.ts
+++ src/types/tasks.ts
@@ -1,8 +1,22 @@
 interface Task {
   _id: string
   title: string
-  description?: Array<any>
+  description?: text
   is_done: boolean
+  parent?: Task._id
+  children?: Task._id[]
 }
 
 export type { Task }
+
+/////////////////////
+//
+// Add these later:
+// type >ref
+// tags >ref
+// status >ref
+// parent >ref
+// children [>ref]
+// assignee >ref
+//
+/////////////////////
